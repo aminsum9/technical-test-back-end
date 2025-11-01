2d07b7d986c48317e4d1a3dc43519f7b91ad5f3f
amin subagiyo <aminsum9@gmail.com>
2025-11-01 16:31:25 +0700

    Add BE-test

---
+++ BE-test/test1.js
@@ -0,0 +1,112 @@
+/**
+ * Direction:
+ * Return a formatted array of sessions with list of classes & students
+ * 
+ * Expected Result:
+ * [
+ *  {
+ *    session_id: 1,
+ *    time: '09:00',
+ *    classes: [
+ *      {
+ *        class_id: 1,
+ *        name: 'A',
+ *        students: [
+ *          { student_id: 1, name: 'Adi' },
+ *          { student_id: 1, name: 'Budi' },
+ *        ],
+ *      },
+ *      {
+ *        class_id: 2,
+ *        name: 'B',
+ *        students: [
+ *          { student_id: 3, name: 'Bayu' },
+ *          { student_id: 4, name: 'Dharma' },
+ *        ],
+ *      },
+ *    ],
+ *  },
+ *  {
+ *    session_id: 2,
+ *    time: '10:00',
+ *    classes: [
+ *      {
+ *        class_id: 3,
+ *        name: 'C',
+ *        students: [
+ *          { student_id: 5, name: 'Surya' },
+ *          { student_id: 6, name: 'Maha' },
+ *        ],
+ *      },
+ *      {
+ *        class_id: 4,
+ *        name: 'D',
+ *        students: [
+ *          { student_id: 7, name: 'Dede' },
+ *          { student_id: 8, name: 'Edi' },
+ *        ],
+ *      },
+ *    ],
+ *  },
+ * ];
+ */
+
+const sessions = [
+  { session_id: 1, time: '09:00', student: { student_id: 1, name: 'Adi' }, class: { class_id: 1, name: 'A' } },
+  { session_id: 2, time: '10:00', student: { student_id: 5, name: 'Surya' }, class: { class_id: 3, name: 'C' } },
+  { session_id: 2, time: '10:00', student: { student_id: 8, name: 'Edi' }, class: { class_id: 4, name: 'D' } },
+  { session_id: 2, time: '10:00', student: { student_id: 7, name: 'Dede' }, class: { class_id: 4, name: 'D' } },
+  { session_id: 1, time: '09:00', student: { student_id: 3, name: 'Bayu' }, class: { class_id: 2, name: 'B' } },
+  { session_id: 1, time: '09:00', student: { student_id: 2, name: 'Budi' }, class: { class_id: 1, name: 'A' } },
+  { session_id: 1, time: '09:00', student: { student_id: 4, name: 'Dharma' }, class: { class_id: 2, name: 'B' } },
+  { session_id: 2, time: '10:00', student: { student_id: 3, name: 'Maha' }, class: { class_id: 3, name: 'C' } },
+];
+
+function result(sessions) {
+  var sessionsItems = [];
+
+ sessions.forEach(e => {
+    if(!sessionsItems.includes(e.session_id)){
+      sessionsItems.push(e.session_id);
+    }
+  });
+
+  sessionsItems = sessionsItems.map(item => {
+    var itemSession = sessions.find(e => e.session_id == item);
+
+    var sessionClasses = sessions.filter(e => {
+      return e.session_id == item
+    });
+
+    var classes = sessionClasses.map(item => {
+
+      var sessionStudents = sessions.filter(e => {
+        return e?.class?.class_id == item?.class?.class_id
+      });
+
+      var students = sessionStudents.map(item => {
+        return { 
+          student_id: item?.student?.student_id, 
+          name: item?.student?.name 
+        }
+      })
+
+      return {
+        class_id: item?.class?.class_id,
+        name: item?.class?.name,
+        students: students
+      }
+    })
+
+    return {
+      session_id: item,
+      time: itemSession.time,
+      classes: classes
+    }
+  })
+
+  return sessionsItems;
+
+}
+
+console.log(result(sessions));
